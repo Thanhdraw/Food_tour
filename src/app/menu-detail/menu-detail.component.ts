096d0f371f09f6d583258bd2b1038c20ac8754b1
Thandraw <dangquocthanh2812@gmail.com>
2023-04-28 17:35:34 +0700

    fixing

--- src/app/menu-detail/menu-detail.component.ts
+++ src/app/menu-detail/menu-detail.component.ts
@@ -1,16 +1,20 @@
 
 import { Food } from '../food';
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { FoodService } from '../food.service';
+import { FOODS } from '../mock-food';
+
+
+
 @Component({
   selector: 'app-menu-detail',
   templateUrl: './menu-detail.component.html',
   styleUrls: ['./menu-detail.component.css']
 })
-export class MenuDetailComponent  implements OnInit {
+export class MenuDetailComponent {
   selectedFood?: Food;
-  productService: any;
+  @Input() food?: Food;
   constructor(private route: ActivatedRoute, private foodService: FoodService) { }
   onSelect(food: Food): void {
     this.selectedFood = food;
